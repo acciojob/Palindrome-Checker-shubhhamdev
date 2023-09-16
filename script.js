b9768292423f8c6f830db9ece1b05b65b849d6cc
Shubham Dev <133051223+shubhhamdev@users.noreply.github.com>
2023-09-16 12:50:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
+	 const cleanedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+
+  // Compare the cleaned string with its reverse
+  const reversedStr = cleanedStr.split('').reverse().join('');
+
+  // Check if the cleaned string is a palindrome
+  return cleanedStr === reversedStr;
 
 }
 module.exports = palindrome
